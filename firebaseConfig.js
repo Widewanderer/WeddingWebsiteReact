4dacdeaf1e527014dc1211d1dbdb2335aea1826a
Widewanderer <slavatrotz@gmail.com>
2024-09-13 15:14:49 -0500

    add compatibility to view firebase database through console.log

---
+++ firebaseConfig.js
@@ -0,0 +1,33 @@
+// firebaseConfig.js
+
+import { initializeApp } from "firebase/app";
+import { child, getDatabase, get, ref } from "firebase/database";
+import constants from "./src/constants";
+
+const firebaseConfig = {
+  apiKey: constants.apiKey,
+  authDomain: "weddingwebsitereact.firebaseapp.com",
+  databaseURL: "https://weddingwebsitereact-default-rtdb.firebaseio.com",
+  projectId: "weddingwebsitereact",
+  storageBucket: "weddingwebsitereact.appspot.com",
+  messagingSenderId: "559812123191",
+  appId: "1:559812123191:web:6bd7e66deb7703f260fdb2",
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+export const database = getDatabase(app);
+
+
+console.log("Firebase initialized");
+
+get(child(ref(database), "Test")).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+})
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { database } from '../firebaseConfig.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
