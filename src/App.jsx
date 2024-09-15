6bcd7a7d31a6d211b873988dbcb8b54a22616eca
Widewanderer <slavatrotz@gmail.com>
2024-09-15 13:31:09 -0500

    replace hero JPG with PNG fPNG  files. add rudementary image for Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/hero'
 
 function App() {
   return (
     <>
-     
+    <Hero />
       <h1 className= "text-3xl font-bold" >Slava & Fiona Wedding </h1>
      
     </>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import Pic7 from '../assets/heroPics/Pic7.png';
+import Pic10 from '../assets/heroPics/Pic10.png';
+import Pic100 from '../assets/heroPics/pic100.png';
+
+
+export default function Hero() {
+
+return (
+    <div className="hero">
+        <img src={Pic7} alt="Home page engagment Photo" /> 
+    </div>
+);
+}
