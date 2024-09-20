6de36d156a83945420a3c440b940d352dea779c5
Widewanderer <slavatrotz@gmail.com>
2024-09-20 12:37:00 -0500

    delete superfluous code in Q&A.jsx

--- src/components/Q&A.jsx
+++ src/components/Q&A.jsx
@@ -2,7 +2,7 @@
 
 export default function QA() {
   return (
-    <div className="p-12 max-w-lg mx-auto bg-beige-200 rounded-md shadow-md flex items-center space-x-4">
+    <div className="p-12 max-w-2xl mx-auto bg-beige-200 rounded-md shadow-md flex items-center space-x-4">
       <div className="shrink-0">
       </div>
       <div>
@@ -14,16 +14,3 @@ export default function QA() {
 }
 
 
-// export default function QA() {
-//     return (
-//     <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-//   <div class="shrink-0">
-//     <img class="size-12" src="/img/logo.svg" alt="ChitChat Logo">
-//   </div>
-//   <div>
-//     <div class="text-xl font-medium text-black">ChitChat</div>
-//     <p class="text-slate-500">You have a new message!</p>
-//   </div>
-// </div>
-// );
-// }
