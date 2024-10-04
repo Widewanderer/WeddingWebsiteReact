54772e4f4b8d43247028a9d0381f45f33e1fc711
Widewanderer <slavatrotz@gmail.com>
2024-10-04 16:43:45 -0500

    add .link styling in app.css for links in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-  
-  // use sticky or absolulte possitioning to keep the navbar at the top of the page and make transparent 
+import '../App.css';
 
 
   function Navbar() {
@@ -10,9 +9,15 @@ import { Link, useLocation } from "react-router-dom";
         <div className="flex justify-between items-center">
           {/* Left side links */}
           <div className="flex space-x-4">
-            <Link to="/">Home</Link>
-            <Link to="/weddingparty">Wedding Party</Link>
-            <Link to="/travel">Travel</Link>
+            <Link to="/" className="link">
+              Home
+            </Link>
+            <Link to="/weddingparty" className="link">
+              Wedding Party
+            </Link>
+            <Link to="/travel" className="link">
+              Travel
+            </Link>
           </div>
           {/* Center logo Icon */}
           <div className="text-center">
@@ -20,15 +25,20 @@ import { Link, useLocation } from "react-router-dom";
           </div>
           {/* Right side links  */}
           <div className="flex space-x-4">
-            <Link to="/rsvp">RSVP</Link>
+            <Link to="/rsvp" className="link">
+              RSVP
+            </Link>
             <a
               href="https://www.zola.com/wedding/slavatysonandfiona"
               target="_blank"
               rel="noopener noreferrer"
+              className="link"
             >
               Registry
             </a>
-            <Link to="/q&a">Q&A</Link>
+            <Link to="/q&a" className="link">
+              Q&A
+            </Link>
           </div>
         </div>
       </nav>
