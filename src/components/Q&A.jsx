37f4d137e0a11f68aab8cfbcf3eceaed79191520
Widewanderer <slavatrotz@gmail.com>
2024-10-07 21:36:29 -0500

    add prompts to the Q&A component and page

--- src/components/Q&A.jsx
+++ src/components/Q&A.jsx
@@ -1,16 +1,15 @@
 
 
-export default function QA() {
+export default function QA(props) {
   return (
-    <div className="p-12 max-w-2xl mx-auto bg-primary flex items-center space-x-4 border-b border-emerald">
+    <div className="p-12 py-20 max-w-2xl mx-auto bg-primary flex items-center space-x-4 border-b border-emerald">
       <div className="shrink-0"></div>
       <div>
         <div className="text-xl font-medium text-emerald">
-          Question(Replace with Variable)
+          {props.question}
         </div>
         <p className="text-slate-500 text-emerald">
-          The answers to all the things that burden your heart and soul.
-          (Replace with variable later)
+          {props.answer}
         </p>
       </div>
     </div>
--- src/pages/Q&APage.jsx
+++ src/pages/Q&APage.jsx
@@ -2,8 +2,15 @@ import QA from '../components/Q&A';
 
 export default function QAPage() {
     return (
-        <div>
-        <QA />
-        </div>
+      <div>
+        <QA
+          question="What is the best movie of all time?"
+          answer="The Two Towers"
+        />
+        <QA
+          question="What is your favorite book?"
+          answer="If the book is a standalone book and not part of a series, then I would say that it is Johnathan Strange and Mr. Norrell."
+        />
+      </div>
     );
     }
