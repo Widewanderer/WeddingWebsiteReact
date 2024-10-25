9066baabda5858253058b071608c5469038946f7
Widewanderer <slavatrotz@gmail.com>
2024-10-25 10:45:33 -0500

    add groomsmen section, file folders for groomsmen and bridesmaid images

--- src/pages/WeddingPartyPage.jsx
+++ src/pages/WeddingPartyPage.jsx
@@ -6,17 +6,25 @@ export default function WeddingPartyPage() {
     return (
       <div>
         <Hero image="/heroPics/Pic2.png" title="Wedding Party" />
-        <Title title="Meet the Wedding Party" />
+        <Title title="The Bridal Party" />
         <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-4">
           <WeddingPartyCard
-            image="/galleryPics/AnimatedFiona.png"
-            title="Test Image"
+            image="/bridalPartyPics/AnimatedFiona.png"
+            title="Bridesmaid 1"
             text="Here is a little test of the wedding party cards component"
           />
           <WeddingPartyCard
-            image="/galleryPics/AnimatedFiona.png"
-            title="Test Image 2"
+            image="/bridalPartyPics/AnimatedFiona.png"
+            title="Bridesmaid 2"
             text="Here is a little test of the wedding party cards component to see how it behaves with multiple cards"
+          />
+        </div>
+        <Title title="The Groomsmen" />
+        <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-4">
+          <WeddingPartyCard
+            image="/groomsmenPics/AnimatedWanderer.png"
+            title="GroomsMan 1"
+            text="Here we have a wild groomsman in his natural habitat.He’s ready to jump into action the moment he’s needed, loyal as they come. Just like a pack animal, he’s here to support the big guy, the groom, standing by his side for the most important day of his life. Absolutely magnificent! "
           />
         </div>
       </div>
