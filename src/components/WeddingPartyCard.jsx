ee08cb8e9b3c25a8f5f9ac315ebc8aafa7d0314c
Widewanderer <slavatrotz@gmail.com>
2024-10-24 22:50:32 -0500

    add flex box controls so that up to 2 WeddingPartyCards can be displayed in each row but only on on small screens.

--- src/components/WeddingPartyCard.jsx
+++ src/components/WeddingPartyCard.jsx
@@ -7,7 +7,7 @@ export default function WeddingPartyCard(props) {
       <img
         src={props.image}
         alt={props.imageAlt}
-        className="w-full h-full object-cover"
+        className="w-full h-2/3 object-cover"
       />
 
       {/* Content section */}
@@ -18,3 +18,22 @@ export default function WeddingPartyCard(props) {
     </div>
   );
 }
+
+// export default function WeddingPartyCard(props) {
+//   return (
+//     <div className="bg-primary rounded-lg overflow-hidden p-6 max-w-xs flex-grow">
+//       {/* Image section */}
+//       <img
+//         src={props.image}
+//         alt={props.imageAlt}
+//         className="w-full h-48 object-cover"
+//       />
+
+//       {/* Content section */}
+//       <div className="p-6 text-center">
+//         <h2 className="text-2xl font-bold text-emerald mb-2">{props.title}</h2>
+//         <p className="text-gray-600">{props.text}</p>
+//       </div>
+//     </div>
+//   );
+// }
--- src/pages/WeddingPartyPage.jsx
+++ src/pages/WeddingPartyPage.jsx
@@ -6,17 +6,19 @@ export default function WeddingPartyPage() {
     return (
       <div>
         <Hero image="/heroPics/Pic2.png" title="Wedding Party" />
-        <Title title="Wedding Party" />
-        <WeddingPartyCard
-          image="/galleryPics/AnimatedFiona.png"
-          title="Test Image"
-          text="Here is a little test of the wedding party cards component"
-        />
-        <WeddingPartyCard
-          image="/galleryPics/AnimatedFiona.png"
-          title="Test Image 2"
-          text="Here is a little test of the wedding party cards component to see how it behaves with multiple cards"
-        />
+        <Title title="Meet the Wedding Party" />
+        <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-4">
+          <WeddingPartyCard
+            image="/galleryPics/AnimatedFiona.png"
+            title="Test Image"
+            text="Here is a little test of the wedding party cards component"
+          />
+          <WeddingPartyCard
+            image="/galleryPics/AnimatedFiona.png"
+            title="Test Image 2"
+            text="Here is a little test of the wedding party cards component to see how it behaves with multiple cards"
+          />
+        </div>
       </div>
     );
     }
