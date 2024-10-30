524ab875ba8dbdeb0566e98ccfde66d62239df62
Widewanderer <slavatrotz@gmail.com>
2024-10-29 21:40:19 -0500

    Refactor Hero to contain a background image rather than an image div. Further work needed to add responsive resizing.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,29 @@
 
 export default function Hero(props) {
   return (
-    <div className="relative w-full shadow-lg">
-      <img
-        src={props.image}
-        alt="Home page engagement Photo"
-        className="w-full h-auto object-cover"
-      />
-      <div className="absolute top-[78%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold text-center">
+    <div
+      className="relative w-full h-[500px] bg-cover bg-center"
+      style={{ backgroundImage: `url(${props.image})` }}
+    >
+      <div className="absolute top-[50%] md:top-[78%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold text-center">
         {props.title}
       </div>
     </div>
   );
 }
+
+
+// export default function Hero(props) {
+//   return (
+//     <div className="relative w-full shadow-lg">
+//       <img
+//         src={props.image}
+//         alt="Home page engagement Photo"
+//         className="w-full h-auto object-cover"
+//       />
+//       <div className="absolute top-[50%] md:top-[78%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold text-center">
+//         {props.title}
+//       </div>
+//     </div>
+//   );
+// }
