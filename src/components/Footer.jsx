88e32743d62afaccc556805009e8cf1cadf99133
Widewanderer <slavatrotz@gmail.com>
2024-11-08 21:54:30 -0600

    add GitHub link tok to footer wt icon using font awesome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,18 @@ export default function Footer() {
     <footer className="bg-EPGreen text-slate-900 p-10 flex justify-between items-center text-black">
       {/* Text aligned to the left */}
       <p>&copy; 2024 Slava & Fiona</p>
-
       {/* Empty div to push the image to the center */}
       <div className="flex-grow"></div>
-
       {/* Image centered */}
       <div className="absolute left-1/2 transform -translate-x-1/2">
         <img src="/react.svg" alt="logo" className="h-10 w-10" />
+      </div>
+      {/* Link to GitHub */}
+      <div>
+        <a href="https://github.com/Widewanderer/WeddingWebsiteReact">
+          <i class="fab fa-github text-3xl"></i>
+          GitHub
+        </a>
       </div>
     </footer>
   );
