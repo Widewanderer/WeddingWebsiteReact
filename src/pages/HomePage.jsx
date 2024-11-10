da8ffb8a29a452cd379b9c708ee6afb6ef6e9c25
Widewanderer <slavatrotz@gmail.com>
2024-11-10 16:21:38 -0600

    adjust position of Nolan Drawings on main page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,26 +6,25 @@ import nolanCat from '../nolanPics/nolanCat.svg'
 import nolanTree from '../nolanPics/nolanTree.svg'
 
 
-
+{
+  /* use as a overlay needs to be view height and view width, put hiden class on mobile view  
+          
+          move this to sit over the title and schedule. Absolute position inside that div */
+}
 export default function HomePage() {
     return (
       <div>
-        <div>
-          {/* use as a overlay needs to be view height and view width, put hiden class on mobile view  
-          
-          move this to sit over the title and schedule. Absolute position inside that div */}
-        </div>
         <Hero image="/heroPics/Pic7.png" title="Slava & Fiona" />
         <div>
           <img
             src={nolanCat}
             alt="Nolan Cat"
-            className="max-w-72 z-index: 1000; absolute top-180 right-0 hidden md:block"
+            className="max-w-72 z-index: 1000; absolute top-[60vh] right-0 hidden md:block"
           />
           <img
             src={nolanTree}
-            alt="Nolan Cat"
-            className="max-w-72 z-index: 1000; absolute top-3000 right-100 hidden md:block"
+            alt="Nolan Tree"
+            className="max-w-72 z-index: 1000; absolute top-[80vh] right-15 hidden md:block"
           />
           <Title title="Socastee, South Carolina" />
           <Schedule
