8759f690b3c2c92f750bd094c34dc4e213d460e5
Widewanderer <slavatrotz@gmail.com>
2024-11-22 16:06:53 -0600

    add crab svg to RSVP page

--- src/pages/RSVPPage.jsx
+++ src/pages/RSVPPage.jsx
@@ -1,13 +1,22 @@
 import Hero from '../components/Hero';
 import RSVPForm from '../components/Form';
+import nolanTree from '../nolanPics/nolanTree.svg'
+import nolanCrab from '../nolanPics/nolanCrab.svg'
 
 
 export default function RSVPPage() {
     return (
+      <div>
+        <Hero image="/heroPics/Pic12.png" title="RSVP" />
         <div>
-            <Hero image='/heroPics/Pic12.png'title='RSVP'/>
-            <RSVPForm/>
+          <img
+            src={nolanCrab}
+            alt="Nolan Crab"
+            className="max-w-72 z-index: 1000; absolute top-[80vh] right-18 hidden lg:block"
+          />
+          <RSVPForm />
         </div>
+      </div>
     );
     }
     
