d366ed9e76ea3179985a5ab19a1acbe6c446ac85
Widewanderer <slavatrotz@gmail.com>
2025-01-10 20:04:08 -0600

    add Michael,Lina, and Faith to bridal party

--- src/pages/WeddingPartyPage.jsx
+++ src/pages/WeddingPartyPage.jsx
@@ -5,17 +5,14 @@ import WeddingPartyCard from '../components/WeddingPartyCard';
 export default function WeddingPartyPage() {
     return (
       <div>
-        <Hero 
-        image="/heroPics/Pic2.png" 
-        title="Wedding Party" 
-        />
+        <Hero image="/heroPics/Pic2.png" title="Wedding Party" />
         <Title title="The Bridal Party" />
         <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-4">
           <WeddingPartyCard
             name="Faith Henthorn"
-            image="/bridalPartyPics/AnimatedFiona.png"
+            image="/bridalPartyPics/Faith.jpg"
             title="Maid of Honor"
-            text="Here is a little test of the wedding party cards component"
+            text="I think the memories that I cherish the most are our little hikes. Where we can talk and laugh for hours without ever growing old of the conversation. Well, as long as we aren't having to dodge falling trees that is."
           />
           <WeddingPartyCard
             name="Emily Henthorn"
@@ -33,17 +30,17 @@ export default function WeddingPartyPage() {
           />
           <WeddingPartyCard
             name="Angelina Khomenok"
-            image="/bridalPartyPics/AnimatedFiona.png"
+            image="/bridalPartyPics/Lina.jpg"
             title="Bridesmaid"
-            text=""
+            text="Seeing Fiona lounging in the backyard of the Eastern European Cultures House at Sewanee on a picnic blanket holding a vintage pink umbrella from the rays of the sun. She looked so calming and charming at the moment that all of my worries and concerns flown away at the sight of my dear friend. "
           />
         </div>
         <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-4">
           <WeddingPartyCard
             name="Michael Stewart "
-            image="../groomsmenPics/AnimatedWanderer.png"
+            image="../bridalParty/Michael.jpg"
             title="Bridesman"
-            text=""
+            text="Fiona’s parents came to visit Sewanee and she invited me to get dinner with her parents by where they were staying in Manchester. We stumbled across Bites of Europe, which featured a very small market of assorted German products. We discussed many things, including which was the croissant story and Fiona’s sign. "
           />
         </div>
         {/* Groomsmen Section */}
