a5983d918af38b62c904ab3f2e4b5cf23f985042
Widewanderer <slavatrotz@gmail.com>
2025-06-13 12:55:55 -0500

    Re-write Friday gathering information due to rain

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,10 +27,9 @@ export default function HomePage() {
             date="June 13, 2025"
             event="Friday Hangout Time"
             time="15:30-17:30"
-            location="WarBird Park"
-            address="Farrow Pkwy, Myrtle Beach, SC 29577"
-            discription="All are welcome to drop by and spend some time with us before the big day! Drop by anytime between 15:30 and 17:30. We’ll be at one of the pavilions there. 
-In case of heavy or prolonged rain, we’ll meet instead at the Socastee UMC Fellowship Hall. Check back here for any weather location updates on Friday morning."
+            location="Socastee United Methodist Old Church"
+            address="5575 Dick Pond Road, Myrtle Beach, SC 29588"
+            discription="All are welcome to drop by and spend some time with us before the big day! The location has been moved inside due to the rain. Drop by anytime between 15:30 and 17:30. We’ll be at the 1875 Church. It is the old small church in front of the cemetery next to the Church where the ceremony will be held tomorrow."
           />
           <Schedule
             date="June 14, 2025"
